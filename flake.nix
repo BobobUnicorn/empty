1a56634a160fe818a3bf763e1eb1130bb6d7b7aa
Cassandra Choi <cassc@google.com>
2024-01-24 20:39:41 +0000

    add empty flake

---
+++ flake.nix
@@ -0,0 +1,11 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    snowfall-lib = {
+      url = "github:snowfallorg/lib";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+}
